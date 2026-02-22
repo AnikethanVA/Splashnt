50c90eb76b343568aee398dc6893071bb4985e45
AnikethanVA <82267223+AnikethanVA@users.noreply.github.com>
2026-02-22 19:19:20 +0530

    Create Ktor HTTP client with JSON serialization and logging

---
+++ app/src/main/java/com/ava/splashnt/data/remote/UnsplashApiClient.kt
@@ -0,0 +1,36 @@
+package com.ava.splashnt.data.remote
+
+import com.ava.splashnt.BuildConfig
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class UnsplashApiClient {
+    val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.INFO
+        }
+
+        defaultRequest {
+            url("https://api.unsplash.com/")
+            header("Authorization", "Client-ID ${BuildConfig.UNSPLASH_ACCESS_KEY}")
+        }
+    }
+}
