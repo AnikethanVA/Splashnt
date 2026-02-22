68c6c345bc859f3c12358fd7a6ba6ca9720ba5a3
AnikethanVA <82267223+AnikethanVA@users.noreply.github.com>
2026-02-22 18:58:18 +0530

    Create Unsplash data models.

---
+++ app/src/main/java/com/ava/splashnt/data/model/UnsplashModel.kt
@@ -0,0 +1,38 @@
+package com.ava.splashnt.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UnsplashModel(
+
+    val id: String,
+
+    val description: String?,
+
+    val urls: UnsplashURLS,
+
+    val user: UnsplashUser
+
+)
+
+@Serializable
+data class UnsplashURLS(
+
+    @SerialName("full")
+    val fullUrl: String,
+
+    @SerialName("regular")
+    val regularUrl: String,
+
+    @SerialName("thumb")
+    val thumbUrl: String
+
+)
+
+@Serializable
+data class UnsplashUser(
+
+    @SerialName("name")
+    val userName: String,
+)
