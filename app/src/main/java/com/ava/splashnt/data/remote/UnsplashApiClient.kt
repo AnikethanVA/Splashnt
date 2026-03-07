c4f9ea4aa3ada5be5c951aa7f407c86acd25e542
AnikethanVA <82267223+AnikethanVA@users.noreply.github.com>
2026-03-08 01:57:34 +0530

    Add retry logic and timeout configuration to Ktor client

--- app/src/main/java/com/ava/splashnt/data/remote/UnsplashApiClient.kt
+++ app/src/main/java/com/ava/splashnt/data/remote/UnsplashApiClient.kt
@@ -5,6 +5,8 @@ import com.ava.splashnt.data.model.UnsplashModel
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.DEFAULT
@@ -15,6 +17,7 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.io.IOException
 import kotlinx.serialization.json.Json
 
 class UnsplashApiClient {
@@ -30,6 +33,18 @@ class UnsplashApiClient {
         install(Logging) {
             logger = Logger.DEFAULT
             level = LogLevel.INFO
+        }
+
+        install(HttpRequestRetry) {
+            maxRetries = 3
+            exponentialDelay()
+            retryOnExceptionIf { _, throwable ->  throwable is IOException }
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 20000
+            socketTimeoutMillis = 20000
+            connectTimeoutMillis = 10000
         }
 
         defaultRequest {
